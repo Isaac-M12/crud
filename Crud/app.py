0d8e51772a79a510fd950ec644cb74864fa7a4e6
Isaac-M12 <112027561+Isaac-M12@users.noreply.github.com>
2023-01-24 21:29:00 -0500

    Add files via upload

---
+++ Crud/app.py
@@ -0,0 +1,112 @@
+from os import environ
+
+from cryptography.fernet import Fernet
+from flask import Flask, jsonify, request, send_file
+from psycopg2 import connect, extras
+
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+app = Flask(__name__)
+key = Fernet.generate_key()
+
+host = 'localhost'
+port = 5432
+dbname = 'crud'
+user = 'postgres'
+password = '1234'
+
+def get_connection():
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
+
+def get_db_connection():
+    conn = connect(host=host, dbname=dbname,
+                user=user, password=password, port=port)
+    return conn
+
+
+@app.get('/api/users')
+def get_users():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(users)
+
+
+@app.post('/api/users')
+def create_user():
+    new_user = request.get_json()
+    username = new_user['username']
+    email = new_user['email']
+    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s) RETURNING *",
+                (username, email, password))
+    new_user = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify(new_user)
+
+
+@app.get('/api/users/<id>')
+def get_user(id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("SELECT * FROM users WHERE id = %s", (id,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+
+    return jsonify(user)
+
+
+@app.put('/api/users/<id>')
+def update_user(id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    new_user = request.get_json()
+    username = new_user['username']
+    email = new_user['email']
+    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+    cur.execute("UPDATE users SET username = %s, email = %s, password = %s WHERE id = %s RETURNING *",
+                (username, email, password, id))
+    updated_user = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    if updated_user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(updated_user)
+
+
+@app.delete('/api/users/<id>')
+def delete_user(id):
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("DELETE FROM users WHERE id = %s RETURNING *", (id,))
+    user = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user)
+
+
+@app.get('/')
+def home():
+    return send_file('static/index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
